f5f0d61481f7931212928e2ccb81ea34689b8f63
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:06:08 +0530

    Create 9_Typecasting.py

---
+++ 9_Typecasting.py
@@ -0,0 +1,15 @@
+
+# Type of TypeCasting :- 1.Explicit TypeCasting      2.Implicit TypeCasting
+
+#Explicit TypeCasting
+a = "1"
+# a = 1
+b = "2"
+# b = 2 
+print(int(a) + int(b)) # convert into int by user
+
+#Implicit TypeCasting
+c = 1.9 #float 
+d = 8  # convert into float by python
+print(c+d) 
+

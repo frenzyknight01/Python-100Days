32a125352862995b5aa51ee2bb44373f96b19159
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:07:24 +0530

    Create 18_While_Loop.py

---
+++ 18_While_Loop.py
@@ -0,0 +1,17 @@
+i = int(input("Enter the number: "))
+while(i <= 38):
+    i = int(input("Enter the number: "))
+    print(i)
+print("DOne with the loop")
+
+count = 15
+while (count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("I am inside else")
+
+
+# do {
+
+# }while()

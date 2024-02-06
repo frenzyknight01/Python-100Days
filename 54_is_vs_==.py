8e56487416a04c354c436b024f24f6fdb8f2ae06
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:16:45 +0530

    Create 54_is_vs_==.py

---
+++ 54_is_vs_==.py
@@ -0,0 +1,13 @@
+a = [1,2,43]
+b = [1,2,43]
+
+print(a is b) #Exact location of object in memory
+print(a == b) #value
+
+
+c = None
+d = None
+
+print(c is d) #exact location of object in memory
+print(c is None) #exact location of object in memory
+print(c == d) #value

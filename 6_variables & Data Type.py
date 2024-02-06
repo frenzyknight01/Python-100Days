a9f13ae2e5a54f9d59016b9345933e93d71d0c7b
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:02:18 +0530

    Create 6_variables & Data Type.py

---
+++ 6_variables & Data Type.py
@@ -0,0 +1,21 @@
+a = complex(8,2)
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is",type(a))
+print("The type of a is",type(b))
+print("The type of a is",type(c))
+print("The type of a is",type(d))
+
+list1 = [1,2,3.1, [5,7], ["paadi", "ronak"]] #sequencial : list are mutabel and can be modified
+print(list1)
+
+tuple = (("parrot", "sparrow"), ("Tiger", "Lion")) #sequencial : tuple are imutable and can not be modified
+print(tuple)
+
+dict1 = {"name":"sakshi", "age":20, "canVote":True} #Map data :dictionary unorder 
+print(dict1)

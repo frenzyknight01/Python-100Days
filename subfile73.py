d20104b5a765ea230d40613b9f60cade98b7c516
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:23:23 +0530

    Create subfile73.py

---
+++ subfile73.py
@@ -0,0 +1,9 @@
+from subfile73 import Employee
+
+e = Employee("Unknown")
+print(e.name)
+
+print(len(e)) #when you create len method then it work 
+print(str(e)) #when you create str method then it work
+print(repr(e)) #when you create repr method then it work 
+e() #when we create call method and accesed it.

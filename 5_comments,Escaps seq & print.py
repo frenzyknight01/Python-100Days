00c3dcdd25cf9420d15e5774b0fe63aad5f20c8a
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:02:07 +0530

    Create 5_comments,Escaps seq & print.py

---
+++ 5_comments,Escaps seq & print.py
@@ -0,0 +1,22 @@
+                            #Comments,Escaps,sequence & print statements
+print("Hey I am a good boy \n and this viewer is also a good boy")
+# Hey don't change this line
+# printing hello world
+
+print("Hello world")  # printing hello world
+
+''' 
+hey don't change this line
+'''
+
+
+"""
+hey don't change this line 
+"""
+
+print("Hey I am a \"good boy\" \n and this viewer is also a good boy") # An escape sequence character is a backslash \ followed by the character you want to insert
+
+print("Hey", 6 , 7)
+print("Hey", 6 , 7 , sep = "~") #For seperate 
+print("Hey", 6 , 7 , sep = "~", end = "009\n")
+

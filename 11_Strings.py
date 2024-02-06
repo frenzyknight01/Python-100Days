8792d27458b13a139a1e67bef1948ccea98201aa
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:07:04 +0530

    Create 11_Strings.py

---
+++ 11_Strings.py
@@ -0,0 +1,22 @@
+name = "Paadi"
+frind = "Meet"
+anotherFriend = 'Ronak'
+apple = ''' He said,
+Hi Paadi
+Hey I am Good 
+"I want to eat an apple '''
+
+print("Hello, " + name)
+#print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+#print(name[5]) # Throw an error 
+
+print("lets use a for loop\n")
+
+for char in apple: # For loop
+    print(char)

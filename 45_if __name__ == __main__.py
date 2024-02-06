8ecedd583651e7ba22d2a9ef1a7fdbd2db91e060
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:11:52 +0530

    Create 45_if __name__ == __main__.py

---
+++ 45_if __name__ == __main__.py
@@ -0,0 +1,14 @@
+# def welcome():
+#     print("Hey you are welcome from frenzy")
+
+# welcome()
+
+
+def welcome():
+    print("Hey you are welcome from frenzy")
+
+print(__name__)
+if __name__ == "__main__":
+    welcome()
+
+

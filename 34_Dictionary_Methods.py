66652ddd190a9a3ccc52733a0229b4acad6cf169
Frenzy <chavdameet2004@gmail.com>
2024-02-06 16:09:46 +0530

    Create 34_Dictionary_Methods.py

---
+++ 34_Dictionary_Methods.py
@@ -0,0 +1,10 @@
+                    #Dictionary Method
+ep1 = {122: 45, 123:89, 567:69, 670:69}
+ep2 = {222:67, 566:90}
+
+ep1.update(ep2) #It can update and merge the dictionary
+ep1.clear() #It clear specific dictionary
+ep1.pop(122) #pop the element
+ep1.popitem() #Popped at the end of the key-value
+del ep1 #It can delete entire dectionary and specific element also
+print(ep1)

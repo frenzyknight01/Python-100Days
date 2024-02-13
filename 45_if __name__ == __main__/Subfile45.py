778847d5917c31aff477fc51a3fe2e567b57e8a9
Frenzy <chavdameet2004@gmail.com>
2024-02-13 12:57:13 +0530

    Update Subfile45.py

--- 45_if __name__ == __main__/Subfile45.py
+++ 45_if __name__ == __main__/Subfile45.py
@@ -1,12 +1,12 @@
-                    #if "__name__ == "__main__"
-'''
-The if __name__ == "__main__" idiom is a common pattern used in Python scripts to determine whether the script is being run directly or being imported as a module into another script.
+# def welcome():
+#     print("Hey you are welcome from frenzy")
 
-In Python, the __name__ variable is a built-in variable that is automatically set to the name of the current module. When a Python script is run directly, the __name__ variable is set to the string __main__ When the script is imported as a module into another script, the __name__ variable is set to the name of the module.
-'''
+# welcome()
 
-import Subfile45 # when we print without using __name__  it can print two time means auto executed
 
-#When we print using __name__ it can print only one time
+def welcome():
+    print("Hey you are welcome from frenzy")
 
-Subfile45.welcome() 
+print(__name__) 
+if __name__ == "__main__":
+    welcome()
